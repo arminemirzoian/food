b821b2555c0efa48c3d123d2277499fddbf503af
arminemirzoian <arminemirzoian@gmail.com>
2021-07-06 10:04:06 +0400

    menu added

--- js/script.js
+++ js/script.js
@@ -98,16 +98,19 @@ setClock('.timer', deadline);
             modal = document.querySelector('.modal'),
             modalCloseBtn = modal.querySelector('[data-close]');
 
-    modalTrigger.forEach(btn => {
-        btn.addEventListener('click', openModal);
-    });
-
+    
     function openModal() {
         modal.classList.add('show');
         modal.classList.remove('hide');
         document.body.style.overflow = "hidden";
         clearInterval(modalTimerId);
     }
+    
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', openModal);
+    });
+
+    
     
     function closeModal() {
         modal.classList.add('hide');
@@ -129,7 +132,7 @@ setClock('.timer', deadline);
         }
     });
 
-    const modalTimerId = setTimeout(openModal, 3000);
+    // const modalTimerId = setTimeout(openModal, 3000);
 
     function showModalByScroll (){
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -138,6 +141,197 @@ setClock('.timer', deadline);
         }
     } 
     window.addEventListener('scroll', showModalByScroll);
+
+    //Menu, classes for menu cards
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.parent = document.querySelector(parentSelector);
+            this.price = price;
+            this.classes = classes;
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+        render() {
+            const element = document.createElement('div');
+            if(this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach( i => element.classList.add(i));
+            }
+            element.innerHTML = `      
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>`;
+            this.parent.append(element);
+        }
+    }
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        `Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!`,
+        9,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        `В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!`,
+        10,
+        ".menu .container"
+
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        `Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!`,
+        12,
+        ".menu .container"
+    ).render();
 });
 
+
+// Constructors
+
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function () {
+//         console.log(`Hello ${this.name}`);
+//     }
+// }
+
+// const ivan = new User('Ivan', 24);
+// const alex = new User('Alex', 26); // ստացանք նոր օբյեկտ նշված հատկանիշներով
+// console.log(ivan); // User { name: 'Ivan', id: 24, human: true, hello: [Function (anonymous)] }
+// ivan.hello();
+
+// User.prototype.exit = function() {
+//     console.log(`User ${this.name} left`); // ստեղծումա նոր մեթոդ ընդհանուր User-ում
+// }
+
+// ivan.exit();
+
+// սովորական ֆունկցիայի դեպքում this == window, use strict-ի դեպքում undefined
+// օբյեկտի մեթոդի կոնտեքսը հենց օբյեկտն է։ Եթե օբյեկտի մեթոդի մեջ ֆունկցիա ենք կանչում, ապա այդ ֆունկցիայի this-ը կլինի undefined, իսկ մեթոդի դեպքում օբյեկտը
+// constructor-ում և class-երում նշված this-ը նոր օբյեկտի օրինակն է
+// ձեռքով կպցնում ենք this-ը call, apply, bind-ի միջոցով
+
+// function showThis (a, b){
+// function sum() {
+//     return this.a + this.b; // undefined, պետք է գրել առանց this-ի, որ ծնողից վերցնի և ցույց տա 9
+// }
+// }
+
+// showThis(4, 5);
+
+// const obj = {
+//     a: 20,
+//     b: 30,
+//     sum: function() {
+//         console.log(this);
+//     }
+// };
+
+// obj.sum(); //{ a: 20, b: 30, sum: [Function: sum] }
+
+// function sayName () {
+//     console.log(this);
+//     console.log(this.name);
+// }
+
+// const user = {
+//     name: 'John'
+// }
+// sayName.call(user);
+// sayName.apply(user); // { name: 'John' } John
+
+
+// function sayName (surname) {
+//     console.log(this);
+//     console.log(this.name + surname);
+// }
+// const user = {
+//     name: 'John'
+// }
+// sayName.call(user, "Smith");
+// sayName.apply(user, ['Smith']); // { name: 'John' } JohnSmith
+
+// function count(num) {
+//     return this *num;
+// }
+
+// const double = count.bind(2); // գրեցինք դաբլ ֆունկցիան, որը ստացավ 2, որը count-ի this-ն է
+// console.log(double(3)); // 6
+
+// btn.addEventListener('click', function () {
+//     console.log(this); // կբերի button-ը սովորական նշված ֆունկցիայի դեպքում, այստեղ this-ը նույննա, ինչ event.target-ը
+// });
+// btn.addEventListener('click', () => {
+//     console.log(this); // կբերի undefined, որովհետև aroow function-ը չունի իր կոնտեքստի կանչը
+// });
+
+// const obj = {
+//     num: 5,
+//     sayNumber: function () {
+//         const say = () => {
+//             console.log(this);
+//         };
+//         say();
+//     }
+// };
+
+// obj.sayNumber(); //{ num: 5, sayNumber: [Function: sayNumber] } arrow function-ները չունեն իրենց կոնտեքստի կանչը, վերցնում է իր ծնողի կոնտեքստը
+
+// const double = a => a * 2;
+// console.log(double(4)); //8
+
+// Class
+// class Rectangle {
+//     constructor (height, width){
+//         this.height = height;
+//         this.width = width;
+//     }
+//     calcArea() { // method
+//         return this.height * this.width;
+//     }
+// }
+
+// class ColoredRectangleWithText extends Rectangle{
+//     constructor(height, width, text, bgColor) {
+//         super(height, width);
+//         this.text = text;
+//         this.bgColor = bgColor;
+
+// }
+// showMyProps() {
+//     console.log(`Text: ${this.text}, color: ${this.bgColor}`);
+// }
+// }
+
+// const div = new ColoredRectangleWithText(10, 20, "Hello", "red");
+// div.showMyProps(); // Text: Hello, color: red
+// console.log(div.calcArea()); //200
+
+// const square = new Rectangle(5, 4);
+// console.log(square.calcArea()); // 20
 
